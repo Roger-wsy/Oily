fd721f2108c15069537eaa25b144e23a7eb69672
WeiJie <chiaweijie2001@gmail.com>
2022-06-11 21:35:32 +0800

    extract NIK

--- lib/index.dart
+++ lib/index.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:oily/screen/Home.dart';
 import 'package:oily/screen/Profile.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Index extends StatefulWidget {
   const Index({Key? key}) : super(key: key);
@@ -12,20 +16,21 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
-  int number =  0;
+  int number = 0;
+  String _text = "";
+  XFile? imageFile;
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> pages = [
-      Home(),
-      Profile()
-    ];
+    List<Widget> pages = [Home(), Profile()];
 
     return Scaffold(
       body: pages[number],
-
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xFFEEA4CE),
-        onPressed: (){},
+        onPressed: () {
+          getImage(ImageSource.camera);
+        },
         child: Container(
           width: 60,
           height: 60,
@@ -35,11 +40,8 @@ class _IndexState extends State<Index> {
           ),
           decoration: BoxDecoration(
               shape: BoxShape.circle,
-              gradient: LinearGradient(colors: [
-                Color(0xFF92A3FD),
-                Color(0xFF9DCEFF)
-              ])
-          ),
+              gradient: LinearGradient(
+                  colors: [Color(0xFF92A3FD), Color(0xFF9DCEFF)])),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -51,17 +53,50 @@ class _IndexState extends State<Index> {
         iconSize: 30,
         backgroundColor: Color.fromRGBO(255, 255, 255, 1),
         currentIndex: number,
-
-        onTap: (value){
+        onTap: (value) {
           setState(() {
             number = value;
           });
         },
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: ""),
-          BottomNavigationBarItem(icon: Icon(Icons.person_rounded),label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.person_rounded), label: ""),
         ],
       ),
     );
+  }
+
+  void getImage(ImageSource source) async {
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        imageFile = pickedImage;
+        setState(() {});
+        getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      imageFile = null;
+      setState(() {});
+      _text = 'Error';
+    }
+  }
+
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognizedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    _text = "";
+    final regex = RegExp(
+        r'(1[1-9]|21|[37][1-6]|5[1-3]|6[1-5]|[89][12])\d{2}\d{2}([04][1-9]|[1256][0-9]|[37][01])(0[1-9]|1[0-2])\d{2}\d{4}',
+        multiLine: true);
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
+        if (regex.hasMatch(line.text)) {
+          _text = line.text;
+        }
+      }
+    }
+    setState(() {});
   }
 }
